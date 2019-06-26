edda1d25684b0d88049abc5ae16feb0cf1f2858a
Al Graham <agraham@redhat.com>
2019-06-26 13:59:18 +0100

    adding some more places in Swindon

--- public/locations.js
+++ public/locations.js
@@ -1,73 +1,145 @@
-var locations = [{
-        "city": "London",
-        "country": "UK",
-        "name": "The Hydrant",
-        "address": "Equitable House, 1 Monument St, London EC3R 8BG",
-        "coordinates": [51.510429, -0.086304],
-        "description": {
-            "Comment": "London Office local pub, will find folk there most nights."
-        },
-        "type": "pub",
-        "link": { "homepage": "https://www.thehydrant.co.uk" }
-    },
+var locations = [
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The Hydrant",
+    "address": "Equitable House, 1 Monument St, London EC3R 8BG",
+    "coordinates": [51.510429, -0.086304],
+    "description":
     {
-        "city": "London",
-        "country": "UK",
-        "name": "Premier Inn London Bank (Tower)",
-        "address": "20 St Mary at Hill, London EC3R 8EE",
-        "coordinates": [51.509623, -0.083528],
-        "description": {
-            "Comment": "Close hotel to office, not usually too expensive."
-        },
-        "type": "hotel",
-        "link": { "homepage": "https://www.premierinn.com/gb/en/hotels/england/greater-london/london/london-bank-tower.html?cid=GLBC_LONMON" }
+        "Comment": "London Office local pub, will find folk there most nights."
     },
+    "type": "pub",
+    "link":
     {
-        "city": "London",
-        "country": "UK",
-        "name": "Black Sheep Coffee",
-        "address": "31 Eastcheap, London EC3M 1DE",
-        "coordinates": [51.510704, -0.083829],
-        "description": {
-            "Comment": "Nice coffee, close to office, 'nuff said."
-        },
-        "type": "coffee",
-        "link": { "homepage": "https://leavetheherdbehind.com" }
-    },
-    {
-        "city": "London",
-        "country": "UK",
-        "name": "Brewdog Tower Hill",
-        "address": "21 Great Tower St, London EC3R 5AR",
-        "coordinates": [51.509916, -0.080754],
-        "description": {
-            "Comment": "An alternate local for the London Office, worth a visit."
-        },
-        "type": "pub",
-        "link": { "homepage": "https://www.brewdog.com/bars/uk/brewdog-outpost-tower-hill" }
-    },
-    {
-        "city": "London",
-        "country": "UK",
-        "name": "London Office",
-        "address": "Peninsular House, 30-36 Monument St, London EC3R 8NB",
-        "coordinates": [51.509621, -0.085278],
-        "description": {
-            "Comment": "Red Hat London Office"
-        },
-        "type": "office",
-        "link": { "homepage": "https://www.redhat.com/en/about/office-locations" }
-    },
-    {
-        "city": "Farnborough",
-        "country": "UK",
-        "name": "Farnborough Office",
-        "address": "200 Fowler Avenue Farnborough, Hampshire GU14 7JP",
-        "coordinates": [51.283309, -0.75775],
-        "description": {
-            "Comment": "Red Hat Farnborough Office, often abbreviated to FAB"
-        },
-        "type": "office",
-        "link": { "homepage": "https://www.redhat.com/en/about/office-locations" }
+        "homepage": "https://www.thehydrant.co.uk"
     }
-]
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "Premier Inn London Bank (Tower)",
+    "address": "20 St Mary at Hill, London EC3R 8EE",
+    "coordinates": [51.509623, -0.083528],
+    "description":
+    {
+        "Comment": "Close hotel to office, not usually too expensive."
+    },
+    "type": "hotel",
+    "link":
+    {
+        "homepage": "https://www.premierinn.com/gb/en/hotels/england/greater-london/london/london-bank-tower.html?cid=GLBC_LONMON"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "Black Sheep Coffee",
+    "address": "31 Eastcheap, London EC3M 1DE",
+    "coordinates": [51.510704, -0.083829],
+    "description":
+    {
+        "Comment": "Nice coffee, close to office, 'nuff said."
+    },
+    "type": "coffee",
+    "link":
+    {
+        "homepage": "https://leavetheherdbehind.com"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "Brewdog Tower Hill",
+    "address": "21 Great Tower St, London EC3R 5AR",
+    "coordinates": [51.509916, -0.080754],
+    "description":
+    {
+        "Comment": "An alternate local for the London Office, worth a visit."
+    },
+    "type": "pub",
+    "link":
+    {
+        "homepage": "https://www.brewdog.com/bars/uk/brewdog-outpost-tower-hill"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "London Office",
+    "address": "Peninsular House, 30-36 Monument St, London EC3R 8NB",
+    "coordinates": [51.509621, -0.085278],
+    "description":
+    {
+        "Comment": "Red Hat London Office"
+    },
+    "type": "office",
+    "link":
+    {
+        "homepage": "https://www.redhat.com/en/about/office-locations"
+    }
+},
+{
+    "city": "Farnborough",
+    "country": "UK",
+    "name": "Farnborough Office",
+    "address": "200 Fowler Avenue Farnborough, Hampshire GU14 7JP",
+    "coordinates": [51.283309, -0.75775],
+    "description":
+    {
+        "Comment": "Red Hat Farnborough Office, often abbreviated to FAB"
+    },
+    "type": "office",
+    "link":
+    {
+        "homepage": "https://www.redhat.com/en/about/office-locations"
+    }
+},
+{
+    "city": "Swindon",
+    "country": "UK",
+    "name": "Helen Browning's Chop House",
+    "address": "19-21 Wood St, Swindon SN1 4AN",
+    "coordinates": [51.552380, -1.775865],
+    "description":
+    {
+        "Comment": "Decent food, meat & 2 veg, but slightly fancy"
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "http://helenbrowningschophouse.co.uk"
+    }
+},
+{
+    "city": "Swindon",
+    "country": "UK",
+    "name": "Los Gatos",
+    "address": "1-3 Devizes Rd, Swindon SN1 4BJ",
+    "coordinates": [51.552278, -1.776904],
+    "description":
+    {
+        "Comment": "Good tapas"
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "http://losgatos.co.uk"
+    }
+},
+{
+    "city": "Swindon",
+    "country": "UK",
+    "name": "The Jewel in the Crown",
+    "address": "14-16 Victoria Rd, Swindon SN1 3AL",
+    "coordinates": [51.557879, -1.779213],
+    "description":
+    {
+        "Comment": "Decent indian restaurant"
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "http://www.jewelinthecrown.co.uk"
+    }
+}]
