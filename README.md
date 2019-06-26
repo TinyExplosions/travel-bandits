4cb1a9dd0b5719331d86a1ffab95d8c179ddf355
Al Graham <agraham@redhat.com>
2019-06-26 09:59:44 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # travel-bandits
 UKI Services list of recommended places to eat, drink, sleep, visit, etc
+
+
+## Good to know
+* `coordinates` can be got by inputting address at [latlong.net](https://www.latlong.net/convert-address-to-lat-long.html)
+* `type` should be one of `office`, `hotel`, `restaurant`, `pub`
---
+++ index.js
@@ -0,0 +1,27 @@
+const path = require('path')
+const fastify = require('fastify')({
+  logger: true
+})
+
+fastify.register(require('fastify-static'), {
+  root: path.join(__dirname, 'public'),
+  prefix: '/public/', // optional: default '/'
+})
+
+fastify.get('/another/path', function (req, reply) {
+  
+})
+
+fastify.get('/', async (request, reply) => {
+  reply.sendFile('index.html') // serving path.join(__dirname, 'public', 'myHtml.html') directly
+})
+
+// fastify.get('/', async (request, reply) => {
+//   reply.type('application/json').code(200)
+//   return { hello: 'world' }
+// })
+
+fastify.listen(8080, (err, address) => {
+  if (err) throw err
+  fastify.log.info(`server listening on ${address}`)
+})
