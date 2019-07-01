00d8bc1fc9137e02a6d6775263884c541a0e2ed3
Al Graham <agraham@redhat.com>
2019-07-01 20:24:18 +0100

    adding offline page

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v3';
+const CACHE_NAME = 'static-cache-v4';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
