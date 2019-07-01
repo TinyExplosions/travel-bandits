59c2054b2530ed3139e00036f8c4ee45d3698c4a
Al Graham <agraham@redhat.com>
2019-07-01 19:46:42 +0100

    updating cace

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v2';
+const CACHE_NAME = 'static-cache-v3';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
