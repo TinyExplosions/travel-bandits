7392881e7dbac5b375083cfbc5e43e519f8834d0
Al Graham <agraham@redhat.com>
2019-07-02 19:57:51 +0100

    merging Dan's changes

--- public/locations.js
+++ public/locations.js
@@ -142,4 +142,227 @@ var locations = [
     {
         "homepage": "http://www.jewelinthecrown.co.uk"
     }
+},{
+    "city": "Guildford",
+    "country": "UK",
+    "name": "The Hub on the Park",
+    "address": "Occam Rd, Guildford, GU2 7YG",
+    "coordinates": [51.240065, -0.612306],
+    "description":
+    {
+        "Comment": "Central Cafe on Surrey Research Park"
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "https://surrey-research-park.com/"
+    }
+},
+{
+    "city": "Guildford",
+    "country": "UK",
+    "name": "Holiday Inn Guildford",
+    "address": "Egerton Rd, Guildford, GU2 7XZ",
+    "coordinates": [51.237079, -0.602972],
+    "description":
+    {
+        "Comment": "Closest Hotel to Surrey Business Park."
+    },
+    "type": "hotel",
+    "link":
+    {
+        "homepage": "https://www.ihg.com/holidayinn/hotels/gb/en/guildford/guisu/hoteldetail"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The Hoxton",
+    "address": "32-40 Blackfriars Rd, South Bank, London SE1 8PB",
+    "coordinates": [51.505786, -0.104743],
+    "description":
+    {
+        "Comment": "Small, quirky, design driven hotel near Blackfriars Bridge. Handy for the IBM Southbank Office."
+    },
+    "type": "hotel",
+    "link":
+    {
+        "homepage": "https://thehoxton.com"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "IBM South Bank Office",
+    "address": "76/78 Upper Ground, Lambeth, London SE1 9PZ",
+    "coordinates": [51.507179, -0.112412],
+    "description":
+    {
+        "Comment": "IBM Southbank Office."
+    },
+    "type": "office",
+    "link":
+    {
+        "homepage": "https://www.ibm.com/uk-en/marketing/locations/sbnk.html"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "Meson Don Felipe",
+    "address": "53 The Cut, South Bank, London SE1 8LF",
+    "coordinates": [51.502963, -0.107174],
+    "description":
+    {
+        "Comment": "Excellent Spanish Tapas Bar, with good tapas and decent selection of sherries. Great for post work, pre-train food and drinks."
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "http://www.mesondonfelipe.com/"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The Anchor and Hope",
+    "address": "36 The Cut, Lambeth, London SE1 8LP",
+    "coordinates": [51.503462, -0.106577],
+    "description":
+    {
+        "Comment": "Gastropub on The Cut, awesome food from a daily changing menu, nice wines, good bustling atmosphere. No bookings."
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "http://www.anchorandhopepub.co.uk/"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The Britannia",
+    "address": "44 Kipling St, Kipling Estate, London SE1 3RU",
+    "coordinates": [51.501207, -0.087120],
+    "description":
+    {
+        "Comment": "Local boozer with huge whisky selection"
+    },
+    "type": "pub",
+    "link":
+    {
+        "homepage": "https://www.onlypub.london/the-britannia"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The Old Kings Head",
+    "address": "King's Head Yard, Greater London SE1 1NA",
+    "coordinates": [51.504793, -0.089729],
+    "description":
+    {
+        "Comment": "Small, hidden away local pub in the heart of the London Bridge/Borough Market area. "
+    },
+    "type": "pub",
+    "link":
+    {
+        "homepage": "http://www.theoldkingshead.uk.com/"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The George",
+    "address": "The George Inn Yard, 77 Borough High St, London SE1 1NH",
+    "coordinates": [51.504191, -0.090026],
+    "description":
+    {
+        "Comment": "Old 17th Century Inn/Pub with original coach house features and courtyard which is excellent in the Summer. Only complaint is the beer (Greene King)."
+    },
+    "type": "pub",
+    "link":
+    {
+        "homepage": "https://www.greeneking-pubs.co.uk/pubs/greater-london/george-southwark/"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "The Blue Eyed Maid",
+    "address": "173 Borough High St, London SE1 1HR",
+    "coordinates": [51.502609, -0.091984],
+    "description":
+    {
+        "Comment": "Late night pub/bar open until 2am every day."
+    },
+    "type": "pub",
+    "link":
+    {
+        "homepage": "https://blueeyedmaid.co.uk/"
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "Cafe Rossi",
+    "address": "57 Borough High St, London SE1 1NE",
+    "coordinates": [51.504669, -0.089925],
+    "description":
+    {
+        "Comment": "From breakfast falafel to a late night Kebab, always open and good too."
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": ""
+    }
+},
+{
+    "city": "London",
+    "country": "UK",
+    "name": "Shrigley's Moroccan Cuisine",
+    "address": "125 Borough High St, London SE1 1NP",
+    "coordinates": [51.503671, -0.091207],
+    "description":
+    {
+        "Comment": "Great little cafe for Moroccan food at lunchtime."
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": ""
+    }
+},
+{
+    "city": "Woking",
+    "country": "UK",
+    "name": "Stovell's",
+    "address": "125 Windsor Rd, Chobham, Woking GU24 8QS",
+    "coordinates": [51.358213, -0.608532],
+    "description":
+    {
+        "Comment": "Vaguely poncy, 4 Rosette restaurant in Chobham. Good food and wine, if a touch lacking in ambience IMHO."
+    },
+    "type": "restaurant",
+    "link":
+    {
+        "homepage": "http://www.stovells.com/"
+    }
+},
+{
+    "city": "Camberley",
+    "country": "UK",
+    "name": "Crown & Cushion",
+    "address": "Minley Rd, Blackwater, Camberley GU17 9UA",
+    "coordinates": [51.307136, -0.804197],
+    "description":
+    {
+        "Comment": "Allegedly Haunted Pub. Decent enough food."
+    },
+    "type": "pub",
+    "link":
+    {
+        "homepage": "https://www.baronspubs.com/crownandcushion/"
+    }
 }]
--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // Update cache names any time any of the cached files change.
-const CACHE_NAME = 'static-cache-v4';
+const CACHE_NAME = 'static-cache-v5';
 
 // Add list of files to cache here.
 const FILES_TO_CACHE = [
